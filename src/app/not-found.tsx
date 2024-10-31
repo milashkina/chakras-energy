9b6bc8b469d31d6d11a70c4bc43bb89a6fe04040
milashkina <milakina.alena.developer@gmail.com>
2024-10-31 14:09:38 +0300

    fix repo file

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function notFound() {
     return (
         <div className={`flex flex-col gap-10 p-10 items-center`}>
             <span>sorry 404 page</span>
-            <Link href={"/"} className={`underline hover:decoration-indigo-500`}>Home -></Link>
+            <Link href={"/"} className={`underline hover:decoration-indigo-500`}>Home</Link>
         </div>
     )
 }
